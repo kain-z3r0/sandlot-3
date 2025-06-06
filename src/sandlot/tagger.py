e8c89d664bab9056bc8520b43eae114b78a6a2be
Neuromancer <Neuromancer@matrix.com>
2025-06-06 11:16:39 -0700

    Deleted tagger.py. Will redo branch and tagger.py

--- src/sandlot/tagger.py
+++
@@ -1,6 +0,0 @@
-from pattern_handler import PatternHandler
-
-def _build_inning_uid(inning: str) -> str:
-    half = PatternHandler("inning_half").search(inning).group("half").upper()
-    num = PatternHandler("inning_num").search(inning).group("num")
-    return f"type=inning,half={half},num={num},"
