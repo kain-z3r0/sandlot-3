858304c0de876ad08b90f12b6917e1f596976dae
Neuromancer <Neuromancer@matrix.com>
2025-06-24 22:21:23 -0700

    updated and the phrase mapping is working, about to update it

--- src/sandlot/tagger.py
+++ src/sandlot/tagger.py
@@ -99,7 +99,6 @@ def rewriter(text: str) -> str:
 
 from itertools import zip_longest
 
-from itertools import zip_longest
 
 def add_abid(text: str) -> str:
     lines = text.splitlines()
@@ -122,6 +121,7 @@ def add_abid(text: str) -> str:
     result.extend(process_atbat_block(atbat_block))
     return "\n".join(result)
 
+
 def process_atbat_block(lines: list[str]) -> list[str]:
     output = []
     for l1, l2 in zip_longest(*[iter(lines)] * 2):
@@ -133,9 +133,6 @@ def process_atbat_block(lines: list[str]) -> list[str]:
             entry = l2.removeprefix("entry=atbat").lstrip(", ")
             output.append(f"entry=atbat_outcome, abid={abid}, {entry}")
     return output
-
-
-
 
 
 sample_text = (
@@ -159,22 +156,20 @@ def pitch_counter(text: str) -> str:
         strikes = 0
         fouls = 0
         for word in line.split(","):
-
-            balls += (1 if word.startswith(" Ball") else 0)
-            fouls += (1 if word.startswith(" Foul") else 0)
-            strikes += (1 if word.startswith(" Strike") else 0)
+            balls += 1 if word.startswith(" Ball") else 0
+            fouls += 1 if word.startswith(" Foul") else 0
+            strikes += 1 if word.startswith(" Strike") else 0
             if word.startswith(" Foul"):
-                strikes += (1 if strikes < 2 else 0)
+                strikes += 1 if strikes < 2 else 0
 
         count = f"{balls}-{strikes}"
         updated_line = ball_pattern.sub("", line).replace(".", "")
         updated_line = strike_pattern.sub("", updated_line)
         updated_line = foul_pattern.sub("", updated_line)
         result.append(f"{updated_line}, pitch_count={count}")
-    
-
 
     return "\n".join(result)
+
 
 def tag_batter(text: str) -> str:
     lines = text.splitlines()
@@ -192,6 +187,7 @@ def tag_batter(text: str) -> str:
 
     return "\n".join(result)
 
+
 def tag_outcome(text: str) -> str:
     result = []
     lines = text.splitlines()
@@ -206,6 +202,7 @@ def tag_outcome(text: str) -> str:
             result.append(line)
 
     return "\n".join(result)
+
 
 def in_play(text: str) -> str:
     lines = text.splitlines()
@@ -234,92 +231,140 @@ def find_positions(text: str) -> str:
 
 
 POSITIONS = {
-    'first baseman', 'pitcher', 'second baseman', 'center fielder',
-    'left fielder', 'shortstop', 'catcher', 'third baseman', 'right fielder'
+    "first baseman",
+    "pitcher",
+    "second baseman",
+    "center fielder",
+    "left fielder",
+    "shortstop",
+    "catcher",
+    "third baseman",
+    "right fielder",
 }
 
-def get_outcome(text):
 
+def get_outcome(text):
     lines = text.splitlines()
     pattern = re.compile(r"ab_result=(?P<outcome>.*?)(?=,|player_)")
 
     result = [
-        match.group("outcome") 
-        for line in lines 
-        if (match := pattern.search(line))
+        match.group("outcome") for line in lines if (match := pattern.search(line))
     ]
 
     return set(result)
 
 
+grouped_phrases = {
+    # Singles
+    "singles on a bunt ": "ab_result=single, batted_type=bunt, play_type=battedball, outs_recorded=0",
+    "singles on a fly ball ": "ab_result=single, batted_type=flyball, play_type=battedball, outs_recorded=0",
+    "singles on a ground ball ": "ab_result=single, batted_type=groundball, play_type=battedball, outs_recorded=0",
+    "singles on a hard ground ball ": "ab_result=single, batted_type=groundball, play_type=battedball, batted_strength=hard, outs_recorded=0",
+    "singles on a line drive ": "ab_result=single, batted_type=linedrive, play_type=battedball, outs_recorded=0",
+    "singles on a pop fly ": "ab_result=single, batted_type=popup, play_type=battedball, outs_recorded=0",
 
-ab_result_map = {
-    # Hits
-    "singles": "single",
-    "doubles": "double",
-    "triples": "triple",
-    "homers": "homerun",
-    "hits an inside the park home run": "inside_the_park_homerun",
+    # Doubles
+    "doubles on a fly ball ": "ab_result=double, batted_type=flyball, play_type=battedball, outs_recorded=0",
+    "doubles on a ground ball ": "ab_result=double, batted_type=groundball, play_type=battedball, outs_recorded=0",
+    "doubles on a hard ground ball ": "ab_result=double, batted_type=groundball, play_type=battedball, batted_strength=hard, outs_recorded=0",
+    "doubles on a line drive ": "ab_result=double, batted_type=linedrive, play_type=battedball, outs_recorded=0",
 
-    # Walk/HBP
-    "walks": "walk",
-    "is hit by pitch": "hit_by_pitch",
+    # Triples
+    "triples on a fly ball ": "ab_result=triple, batted_type=flyball, play_type=battedball, outs_recorded=0",
+    "triples on a hard ground ball ": "ab_result=triple, batted_type=groundball, play_type=battedball, batted_strength=hard, outs_recorded=0",
+    "triples on a line drive ": "ab_result=triple, batted_type=linedrive, play_type=battedball, outs_recorded=0",
 
-    # Outs (single)
-    "grounds out": "groundout",
-    "flies out": "flyout",
-    "lines out": "lineout",
-    "pops out": "popup",
-    "out on infield fly": "infield_fly",
-    "out on sacrifice fly": "sacrifice_fly",
-    "is out on foul tip": "foul_tip_out",
-    "out (other)": "out_other",
+    # Home Runs
+    "homers on a fly ball ": "ab_result=homerun, batted_type=flyball, play_type=battedball, outs_recorded=0",
+    "hits an inside the park home run on a fly ball ": "ab_result=homerun, batted_type=flyball, play_type=battedball, outs_recorded=0",
+    "hits an inside the park home run on a hard ground ball ": "ab_result=homerun, batted_type=groundball, play_type=battedball, batted_strength=hard, outs_recorded=0",
 
-    # Outs (multiple)
-    "lines into a double play": "double_play",
-    "flies into a double play": "double_play",
-    "grounds into a double play": "double_play",
-    "grounds into a fielder's choice double play": "double_play",
-    "pops into a double play": "double_play",
-    "grounds into fielder's choice": "fielder_choice",
+    # Walks / HBP
+    "walks ": "ab_result=walk, play_type=non_batted, outs_recorded=0",
+    "is hit by pitch ": "ab_result=hit_by_pitch, play_type=non_batted, outs_recorded=0"
 
     # Reaches on Error
-    "hits a ground ball and reaches on an error": "reaches_on_error_groundball",
-    "hits a fly ball and reaches on an error": "reaches_on_error_flyball",
-    "hits a line drive and reaches on an error": "reaches_on_error_linedrive",
-    "hits a hard ground ball and reaches on an error": "reaches_on_error_hard_groundball",
-    "reaches on dropped 3rd strike (wild pitch)": "reaches_on_error_dropped_third_strike",
-    "reaches on dropped 3rd strike (passed ball)": "reaches_on_error_dropped_third_strike",
+    "hits a ground ball and reaches on an error ": "ab_result=reaches_on_error, batted_type=groundball, play_type=error, outs_recorded=0",
+    "hits a fly ball and reaches on an error ": "ab_result=reaches_on_error, batted_type=flyball, play_type=error, outs_recorded=0",
+    "hits a line drive and reaches on an error ": "ab_result=reaches_on_error, batted_type=linedrive, play_type=error, outs_recorded=0",
+    "hits a hard ground ball and reaches on an error ": "ab_result=reaches_on_error, batted_type=groundball, batted_strength=hard, play_type=error, outs_recorded=0",
+    "reaches on dropped 3rd strike (wild pitch) ": "ab_result=strikeout_dropped_third_strike, play_type=dropped_third_strike, pitch_type=wild_pitch, outs_recorded=0",
+    "reaches on dropped 3rd strike (passed ball) ": "ab_result=strikeout_dropped_third_strike, play_type=dropped_third_strike, pitch_type=passed_ball, outs_recorded=0"
 
     # Strikeouts
-    "strikes out swinging": "strikeout_swinging",
-    "strikes out looking": "strikeout_looking",
-    "out at first on dropped 3rd strike": "strikeout_dropped_third",
+    "strikes out swinging ": "ab_result=strikeout_swinging, play_type=strikeout, outs_recorded=1",
+    "strikes out looking ": "ab_result=strikeout_looking, play_type=strikeout, outs_recorded=1",
+    "out at first on dropped 3rd strike ": "ab_result=strikeout_dropped_third, play_type=dropped_third_strike, outs_recorded=1",
 
-    # Sacrifice
-    "sacrifices": "sacrifice",
+    # Outs
+    "grounds out ": "ab_result=groundout, batted_type=groundball, play_type=battedball, outs_recorded=1",
+    "flies out ": "ab_result=flyout, batted_type=flyball, play_type=battedball, outs_recorded=1",
+    "flies out in foul territory ": "ab_result=flyout, batted_type=flyball, play_type=battedball, foul=True, outs_recorded=1",
+    "lines out ": "ab_result=lineout, batted_type=linedrive, play_type=battedball, outs_recorded=1",
+    "pops out ": "ab_result=popout, batted_type=popup, play_type=battedball, outs_recorded=1",
+    "pops into a double play ": "ab_result=double_play, batted_type=popup, play_type=battedball, outs_recorded=2",
+    "out (other) ": "ab_result=out_other, play_type=fielded_play, outs_recorded=1",
+    "out on sacrifice fly ": "ab_result=sacrifice_fly, batted_type=flyball, play_type=sacrifice, outs_recorded=1",
+    "out on infield fly ": "ab_result=infield_fly, batted_type=popup, play_type=battedball, outs_recorded=1",
+    "is out on foul tip ": "ab_result=strikeout_foul_tip, play_type=strikeout, outs_recorded=1",
+
+    # Double / Multiple Outs
+    "lines into a double play ": "ab_result=double_play, batted_type=linedrive, play_type=battedball, outs_recorded=2",
+    "grounds into a double play ": "ab_result=double_play, batted_type=groundball, play_type=battedball, outs_recorded=2",
+    "grounds into fielder's choice double play ": "ab_result=double_play, play_type=fielder_choice, batted_type=groundball, outs_recorded=2",
+    "grounds into fielder's choice ": "ab_result=fielder_choice, batted_type=groundball, play_type=fielder_choice, outs_recorded=1",
+
+    # Misc
+    "sacrifices ": "ab_result=sacrifice_hit, play_type=sacrifice, outs_recorded=1",
+
+    # Hit locations
+    "to pitcher": "hit_loc=1",
+    "by pitcher": "hit_loc=1",
+    "to catcher": "hit_loc=2",
+    "by catcher": "hit_loc=2",
+    "to first baseman": "hit_loc=3",
+    "by first baseman": "hit_loc=3",
+    "to second baseman": "hit_loc=4",
+    "by second baseman": "hit_loc=4",
+    "to third baseman": "hit_loc=5",
+    "by third baseman": "hit_loc=5",
+    "to shortstop": "hit_loc=6",
+    "by shortstop": "hit_loc=6",
+    "to left fielder": "hit_loc=7",
+    "by left fielder": "hit_loc=7",
+    "to center fielder": "hit_loc=8",
+    "by center fielder": "hit_loc=8",
+    "to right fielder": "hit_loc=9",
+    "by right fielder": "hit_loc=9"
 }
 
-batted_type_map = {
-    "ground ball": "groundball",
-    "hard ground ball": "groundball",
-    "fly ball": "flyball",
-    "flies": "flyball",
-    "pop fly": "popup",
-    "line drive": "linedrive",
-    "lines": "linedrive",
-    "hard line drive": "linedrive",  # if this ever appears
-    "bunt": "bunt",
-}
 
 
 
+def fix_location_formatting(text):
+    pattern = re.compile(
+        r"(ab_result=[^,]+), "
+        r"((?:pitcher|catcher|first baseman|second baseman|third baseman|"
+        r"shortstop|left fielder|center fielder|right fielder))([^,.]*)"
+    )
+
+    result = []
+
+    for line in text.splitlines():
+        if line.startswith("entry=atbat") and (match := pattern.search(line)):
+            result.append(f"{match.group(1)} to {match.group(2)}")
+            # print(f"Full match: {match.group(0)}")
+            # print(f"Modified: {match.group(1)} to {match.group(2)}")
+        else:
+            result.append(line)
+
+    return "\n".join(result)
 
 
-
-                                                                        
-
-
+def find_single(text):
+    pattern = re.compile(r"singles on a fly ball(?! to)")
+    matches = [line for line in text.splitlines() if pattern.search(line)]
+    return set(matches)
 
 
 def main():
@@ -339,11 +384,16 @@ def main():
     b = tag_batter(t)
     c = tag_outcome(b)
     d = in_play(c)
-    pos = find_positions(d)
-    out = get_outcome(d)
-    for o in out:
-        print(o)
+    # pos = find_positions(d)
+    # out = get_outcome(d)
 
+    h = fix_location_formatting(d)
+    # print(h)
+    # for o in out:
+    #     print(o)
+    # singles = find_single(full_text)
+    # for single in singles:
+    #     print(single)
 
 
 if __name__ == "__main__":
